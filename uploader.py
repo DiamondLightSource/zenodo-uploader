cf976fea9d73868a1b22da1b0ec39bd94c5864e3
Graeme Winter <graeme.winter@gmail.com>
2020-03-20 16:35:47 +0000

    Initial refactor (#1)
    * Started to refactor so something beyond a stupid script
    
    * Tidied, works, happy

--- uploader.py
+++ uploader.py
@@ -5,66 +5,139 @@ import json
 import pprint
 
 
-def action_upload(directory):
-    # get upload key from somewhere
+def get_access_token(mode="production"):
+    """get upload key, strip white space."""
 
-    ACCESS_TOKEN = (
-        open(os.path.join(os.environ["HOME"], ".zenodo_id"), "r").read().strip()
-    )
+    if mode == "production":
+        return open(os.path.join(os.environ["HOME"], ".zenodo_id"), "r").read().strip()
+    elif mode == "sandbox":
+        return open(os.path.join(os.environ["HOME"], ".sandbox_id"), "r").read().strip()
 
-    headers = {"Content-Type": "application/json"}
-    r = requests.post(
-        "https://zenodo.org/api/deposit/depositions",
-        params={"access_token": ACCESS_TOKEN},
-        json={},
-        headers=headers,
-    )
 
-    print(r.status_code)
-    print(r.json())
+class zenodo_uploader(object):
+    """tool to upload files to http://zenodo.org"""
 
-    d_id = r.json()["id"]
+    def __init__(self, directory, metadata, mode="production"):
 
-    bucket_url = r.json()["links"]["bucket"]
+        # validate the structure of the metadata - there will be critical
+        # items which must be present for this to be a useful deposition
 
-    # list the upload directory / file list for files to upload
-    for filename in os.listdir(directory):
-        print(filename)
+        if not "metadata" in metadata:
+            raise ValueError("metadata key missing")
 
-        # use new stream-based API
-        with open(os.path.join(directory, filename), "rb") as fin:
-            r = requests.put(
-                "%s/%s" % (bucket_url, filename),
-                data=fin,
-                params={"access_token": ACCESS_TOKEN},
-            )
+        valid = set(("title", "description", "creators", "keywords"))
+        names = set(metadata["metadata"])
+
+        if names - valid:
+            raise ValueError("unknown metadata keys: %s" % (" ".join(names - valid)))
+        if valid - names:
+            raise ValueError("missing metadata keys: %s" % (" ".join(valid - names)))
+
+        # add standard metadata
+
+        metadata["metadata"].update(
+            {
+                "access_right": "open",
+                "upload_type": "dataset",
+                "communities": [{"identifier": "mx"}],
+            }
+        )
+
+        self._directory = directory
+        self._metadata = metadata
+        self._token = get_access_token(mode)
+        self._dep_id = None
+        self._dep_url = None
+        if mode == "production":
+            self._server = "https://zenodo.org"
+        elif mode == "sandbox":
+            self._server = "https://sandbox.zenodo.org"
+
+        for k in "title", "description", "creators", "keywords":
+            print(metadata["metadata"][k])
+
+    def _create(self):
+        """create new empty deposition"""
+        r = requests.post(
+            "%s/api/deposit/depositions" % self._server,
+            params={"access_token": self._token},
+            json={},
+            headers={"Content-Type": "application/json"},
+        )
+
         pprint.pprint(r.json())
+        if not r.status_code in (200, 201, 202, 410):
+            raise RuntimeError("in create: HTTP status %d" % r.status_code)
 
-    # now add some metadata
+        r_json = r.json()
+        self._dep_id = r_json["id"]
+        self._dep_url = r_json["links"]["bucket"]
+
+        print("Created deposition: id = %s" % self._dep_id)
+
+    def _update(self):
+        """push the metadata for this deposition"""
+
+        r = requests.put(
+            "https://zenodo.org/api/deposit/depositions/%s" % self._dep_id,
+            params={"access_token": self._token},
+            data=json.dumps(self._metadata),
+            headers={"Content-Type": "application/json"},
+        )
+
+        if not r.status_code in (200, 201, 202, 410):
+            raise RuntimeError("in create: HTTP status %d" % r.status_code)
+
+        print("Uploaded metadata for: %s" % (self._metadata["metadata"]["title"]))
+
+    def _upload(self, filename):
+        """upload file using stream API"""
+
+        print("Uploading: %s" % filename)
+
+        with open(os.path.join(self._directory, filename), "rb") as fin:
+            r = requests.put(
+                "%s/%s" % (self._dep_url, filename),
+                data=fin,
+                params={"access_token": self._token},
+            )
+
+        if not r.status_code in (200, 201, 202, 410):
+            raise RuntimeError("in create: HTTP status %d" % r.status_code)
+
+        print("Upload complete")
+
+    def _publish(self):
+        """complete the deposition process"""
+        pass
+
+    def upload(self):
+        """process files for upload"""
+
+        # FIXME wrap this in a try except
+
+        self._create()
+        self._update()
+        for filename in os.listdir(self._directory):
+            self._upload(filename)
+        self._publish()
+
+        # and in the except, delete the partial upload as it is broken
+        # - particularly to catch Ctrl-C
+
+
+if __name__ == "__main__":
 
     metadata = {
         "metadata": {
-            "title": "Test automated upload of Eiger data set",
-            "upload_type": "dataset",
+            "title": "Test automated upload of Eiger data set 2",
             "description": """Example data set of cubic insulin recorded on Diamond Light source beamline I04 - being used here as a test upload using the Zenodo REST API. Probably should have things like a deposition ID in here as well as crystallisation condition smiles strings or something.""",
             "creators": [
                 {"name": "Winter, Graeme", "affiliation": "Diamond Light Source"}
             ],
             "keywords": ["automated upload"],
-            "access_right": "open",
-            "communities": [{"identifier": "mx"}],
         }
     }
 
-    r = requests.put(
-        "https://zenodo.org/api/deposit/depositions/%s" % d_id,
-        params={"access_token": ACCESS_TOKEN},
-        data=json.dumps(metadata),
-        headers=headers,
-    )
-
-    print(r.status_code)
-    pprint.pprint(r.json())
-
-
-action_upload(sys.argv[1])
+    zu = zenodo_uploader(sys.argv[1], metadata)
+    zu.upload()
